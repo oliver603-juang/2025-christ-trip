2224c034705359f0ab59db7bc2ae6e0d82504e67
oliver603-juang <oliver603@gmail.com>
2025-12-17 17:31:26 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'jurassic-trip-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  // 修改這裡：改成 icon.png
+  './icon.png',
+  './manifest.json'
+];
+
+// ... (其餘程式碼保持不變) ...
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        const fetchPromise = fetch(event.request).then(
+          (networkResponse) => {
+            if(!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+              return networkResponse;
+            }
+            const responseToCache = networkResponse.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return networkResponse;
+          }
+        ).catch(() => {});
+        return response || fetchPromise;
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
